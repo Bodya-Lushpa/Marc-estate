7f595b0106d33e3e24442f5b8ba266b2fa198291
Bohdan L <bodya.lushpa@gmail.com>
2022-12-20 10:53:33 +0200

    error elfinder10

--- vendor/barryvdh/laravel-elfinder/resources/views/tinymce.php
+++ vendor/barryvdh/laravel-elfinder/resources/views/tinymce.php
@@ -1,82 +1,85 @@
 <!DOCTYPE html>
 <html lang="<?= app()->getLocale() ?>">
-    <head>
-        <meta charset="utf-8">
-        <title>elFinder 2.0</title>
 
-        <!-- jQuery and jQuery UI (REQUIRED) -->
-        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<head>
+	<meta charset="utf-8">
+	<title>elFinder 2.0</title>
 
-        <!-- elFinder CSS (REQUIRED) -->
-        <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/elfinder.min.css') ?>">
-        <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/theme.css') ?>">
+	<!-- jQuery and jQuery UI (REQUIRED) -->
+	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
-        <!-- elFinder JS (REQUIRED) -->
-        <script src="<?= asset($dir.'/js/elfinder.min.js') ?>"></script>
+	<!-- elFinder CSS (REQUIRED) -->
+	<link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/elfinder.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/theme.css') ?>">
 
-        <!-- TinyMCE Popup class (REQUIRED) -->
-        <script type="text/javascript" src="<?= asset($dir.'/js/tiny_mce_popup.js') ?>"></script>
+	<!-- elFinder JS (REQUIRED) -->
+	<script src="<?= asset($dir . '/js/elfinder.min.js') ?>"></script>
 
-        <?php if($locale){ ?>
-            <!-- elFinder translation (OPTIONAL) -->
-            <script src="<?= asset($dir."/js/i18n/elfinder.$locale.js") ?>"></script>
-        <?php } ?>
+	<!-- TinyMCE Popup class (REQUIRED) -->
+	<script type="text/javascript" src="<?= asset($dir . '/js/tiny_mce_popup.js') ?>"></script>
 
-        <script type="text/javascript">
-            var FileBrowserDialogue = {
-                init: function() {
-                    // Here goes your code for setting your custom things onLoad.
-                },
-                mySubmit: function (URL) {
-                    var win = tinyMCEPopup.getWindowArg('window');
+	<?php if ($locale) { ?>
+		<!-- elFinder translation (OPTIONAL) -->
+		<script src="<?= asset($dir . "/js/i18n/elfinder.$locale.js") ?>"></script>
+	<?php } ?>
 
-                    // pass selected file path to TinyMCE
-                    win.document.getElementById(tinyMCEPopup.getWindowArg('input')).value = URL;
+	<script type="text/javascript">
+		var FileBrowserDialogue = {
+			init: function() {
+				// Here goes your code for setting your custom things onLoad.
+			},
+			mySubmit: function(URL) {
+				var win = tinyMCEPopup.getWindowArg('window');
 
-                    // are we an image browser?
-                    if (typeof(win.ImageDialog) != 'undefined') {
-                        // update image dimensions
-                        if (win.ImageDialog.getImageData) {
-                            win.ImageDialog.getImageData();
-                        }
-                        // update preview if necessary
-                        if (win.ImageDialog.showPreviewImage) {
-                            win.ImageDialog.showPreviewImage(URL);
-                        }
-                    }
+				// pass selected file path to TinyMCE
+				win.document.getElementById(tinyMCEPopup.getWindowArg('input')).value = URL;
 
-                    // close popup window
-                    tinyMCEPopup.close();
-                }
-            }
+				// are we an image browser?
+				if (typeof(win.ImageDialog) != 'undefined') {
+					// update image dimensions
+					if (win.ImageDialog.getImageData) {
+						win.ImageDialog.getImageData();
+					}
+					// update preview if necessary
+					if (win.ImageDialog.showPreviewImage) {
+						win.ImageDialog.showPreviewImage(URL);
+					}
+				}
 
-            tinyMCEPopup.onInit.add(FileBrowserDialogue.init, FileBrowserDialogue);
+				// close popup window
+				tinyMCEPopup.close();
+			}
+		}
 
-            $().ready(function() {
-                var elf = $('#elfinder').elfinder({
-                    // set your elFinder options here
-                    <?php if($locale){ ?>
-                        lang: '<?= $locale ?>', // locale
-                    <?php } ?>
-                    customData: { 
-                        _token: '<?= csrf_token() ?>'
-                    },
-                    url : '<?= route("elfinder.connector") ?>',  // connector URL
-                    soundPath: '<?= asset($dir.'/sounds') ?>',
-                    getFileCallback: function(file) { // editor callback
-                        FileBrowserDialogue.mySubmit(file.url); // pass selected file path to TinyMCE
-                    }
-                }).elfinder('instance');
-            });
-        </script>
+		tinyMCEPopup.onInit.add(FileBrowserDialogue.init, FileBrowserDialogue);
 
-    </head>
-    <body>
+		$().ready(function() {
+			var elf = $('#elfinder').elfinder({
+				// set your elFinder options here
+				<?php if ($locale) { ?>
+					lang: '<?= $locale ?>', // locale
+				<?php } ?>
+				customData: {
+					_token: '<?= csrf_token() ?>'
+				},
+				url: '<?= route("elfinder.connector") ?>', // connector URL
+				soundPath: '<?= asset($dir . '/sounds') ?>',
+				getFileCallback: function(file) { // editor callback
+					FileBrowserDialogue.mySubmit(file.url); // pass selected file path to TinyMCE
+				}
+			}).elfinder('instance');
+		});
+	</script>
 
-        <!-- Element where elFinder will be created (REQUIRED) -->
-        <div id="elfinder"></div>
+</head>
 
-    </body>
+<body>
+
+	<!-- Element where elFinder will be created (REQUIRED) -->
+	<div id="elfinder"></div>
+
+</body>
+
 </html>
