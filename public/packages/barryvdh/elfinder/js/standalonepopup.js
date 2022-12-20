be176615e6f1f4353d40cbcf7fc36dfd57d8503f
Bohdan L <bodya.lushpa@gmail.com>
2022-12-20 09:58:12 +0200

    error elfinder4

--- public/packages/barryvdh/elfinder/js/standalonepopup.js
+++ public/packages/barryvdh/elfinder/js/standalonepopup.js
@@ -17,15 +17,14 @@ $(document).on('click','.popup_selector',function (event) {
 // function to update the file selected by elfinder
 function processSelectedFile(filePath, requestingField) {
 	console.log('elfinderTest');
+	console.log(filePath);
+	console.log(requestingField);
 	filePaths = [];
 	filePath.forEach(function(file, index) {
 		filePaths[index] = file.path;
 	});
 	var pefixFiels = requestingField.split('_');
 	var pefixFielsNumber = requestingField.split('g');
-	console.log('elfinderTest');
-	console.log(filePath);
-	console.log(requestingField);
 	// if(pefixFiels[0] == 'plan'){
 	// 		$('.' + requestingField).html(" ");
 	// 		$('.' + requestingField).append( "<div class='uploadImgClose'><img src='/" + filePath[0]['path'] + "' alt='' class='uploadImg d-block mb-4 mr-2 w-100'><input type='text' class='d-none' name=\"plan[" + pefixFielsNumber[1] + "][img]\" value=' /" + filePath[0]['path'] + "'></div>" ).trigger('change');
