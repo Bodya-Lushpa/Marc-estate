8961a9c2d334469843b85d243ffce5fc8605ba82
Bohdan L <bodya.lushpa@gmail.com>
2022-12-20 10:49:41 +0200

    error elfinder8

--- vendor/barryvdh/laravel-elfinder/resources/views/standalonepopup.php
+++ vendor/barryvdh/laravel-elfinder/resources/views/standalonepopup.php
@@ -1,60 +1,67 @@
 <!DOCTYPE html>
 <html lang="<?= app()->getLocale() ?>">
-    <head>
-        <meta charset="utf-8">
-        <title>elFinder 2.0</title>
 
-        <!-- jQuery and jQuery UI (REQUIRED) -->
-        <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/smoothness/jquery-ui.css">
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<head>
+	<meta charset="utf-8">
+	<title>elFinder 2.0</title>
 
-        <!-- elFinder CSS (REQUIRED) -->
-        <link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/elfinder.min.css') ?>">
-        <link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/theme.css') ?>">
+	<!-- jQuery and jQuery UI (REQUIRED) -->
+	<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/smoothness/jquery-ui.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
-        <!-- elFinder JS (REQUIRED) -->
-        <script src="<?= asset($dir . '/js/elfinder.min.js') ?>"></script>
+	<!-- elFinder CSS (REQUIRED) -->
+	<link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/elfinder.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/theme.css') ?>">
 
-        <?php if ($locale)
-        { ?>
-            <!-- elFinder translation (OPTIONAL) -->
-            <script src="<?= asset($dir . "/js/i18n/elfinder.$locale.js") ?>"></script>
-        <?php } ?>
-        <!-- Include jQuery, jQuery UI, elFinder (REQUIRED) -->
+	<!-- elFinder JS (REQUIRED) -->
+	<script src="<?= asset($dir . '/js/elfinder.min.js') ?>"></script>
 
-        <script type="text/javascript">
-            $().ready(function () {
-                var elf = $('#elfinder').elfinder({
-                    // set your elFinder options here
-                    <?php if($locale){ ?>
-                        lang: '<?= $locale ?>', // locale
-                    <?php } ?>
-                    customData: { 
-                        _token: '<?= csrf_token() ?>'
-                    },
-                    url: '<?= route("elfinder.connector") ?>',  // connector URL
-                    soundPath: '<?= asset($dir.'/sounds') ?>',
-                    dialog: {width: 900, modal: true, title: 'Select a file'},
-                    resizable: false,
-                    commandsOptions: {
-                        getfile: {
-                            oncomplete: 'destroy'
-                        }
-                    },
-                    getFileCallback: function (file) {
-                        window.parent.processSelectedFile(file.path, '<?= $input_id?>');
-                        parent.jQuery.colorbox.close();
-                    }
-                }).elfinder('instance');
-            });
-        </script>
+	<?php if ($locale) { ?>
+		<!-- elFinder translation (OPTIONAL) -->
+		<script src="<?= asset($dir . "/js/i18n/elfinder.$locale.js") ?>"></script>
+	<?php } ?>
+	<!-- Include jQuery, jQuery UI, elFinder (REQUIRED) -->
 
-    </head>
-    <body>
+	<script type="text/javascript">
+		$().ready(function() {
+			var elf = $('#elfinder').elfinder({
+				// set your elFinder options here
+				<?php if ($locale) { ?>
+					lang: '<?= $locale ?>', // locale
+				<?php } ?>
+				customData: {
+					_token: '<?= csrf_token() ?>'
+				},
+				url: '<?= route("elfinder.connector") ?>', // connector URL
+				soundPath: '<?= asset($dir . '/sounds') ?>',
+				dialog: {
+					width: 900,
+					modal: true,
+					title: 'Select a file'
+				},
+				resizable: false,
+				commandsOptions: {
+					getfile: {
+						oncomplete: 'destroy'
+					}
+				},
+				getFileCallback: function(file) {
+					window.parent.processSelectedFile(file.path, '<?= $input_id ?>');
+					parent.jQuery.colorbox.close();
+					console.log('test');
+				}
+			}).elfinder('instance');
+		});
+	</script>
 
-        <!-- Element where elFinder will be created (REQUIRED) -->
-        <div id="elfinder"></div>
+</head>
 
-    </body>
+<body>
+
+	<!-- Element where elFinder will be created (REQUIRED) -->
+	<div id="elfinder"></div>
+
+</body>
+
 </html>
