0915ea48472a9259889bf2db94adfa2acd7f5770
Bohdan L <bodya.lushpa@gmail.com>
2022-12-20 11:18:11 +0200

    error elfinder11

--- public/packages/barryvdh/elfinder/js/standalonepopup.js
+++ public/packages/barryvdh/elfinder/js/standalonepopup.js
@@ -16,21 +16,18 @@ $(document).on('click','.popup_selector',function (event) {
 });
 // function to update the file selected by elfinder
 function processSelectedFile(filePath, requestingField) {
-	console.log('elfinderTest');
-	console.log(filePath);
-	console.log(requestingField);
 	filePaths = [];
 	filePath.forEach(function(file, index) {
 		filePaths[index] = file.path;
 	});
 	var pefixFiels = requestingField.split('_');
 	var pefixFielsNumber = requestingField.split('g');
-	// if(pefixFiels[0] == 'plan'){
-	// 		$('.' + requestingField).html(" ");
-	// 		$('.' + requestingField).append( "<div class='uploadImgClose'><img src='/" + filePath[0]['path'] + "' alt='' class='uploadImg d-block mb-4 mr-2 w-100'><input type='text' class='d-none' name=\"plan[" + pefixFielsNumber[1] + "][img]\" value=' /" + filePath[0]['path'] + "'></div>" ).trigger('change');
-	// }else{
-	// 	filePaths.forEach(element => {
-	// 		$('.' + requestingField).append( "<div class='uploadImgClose w-25 mr-2'><img src='/" + element + "' alt='' class='uploadImg d-block mb-4 mr-2 w-100'><input type='text' class='d-none' name='img[]' value=' /" + element + "'></div>" ).trigger('change');
-	// 	});
-	// }
+	if(pefixFiels[0] == 'plan'){
+			$('.' + requestingField).html(" ");
+			$('.' + requestingField).append( "<div class='uploadImgClose'><img src='/" + filePath[0]['path'] + "' alt='' class='uploadImg d-block mb-4 mr-2 w-100'><input type='text' class='d-none' name=\"plan[" + pefixFielsNumber[1] + "][img]\" value=' /" + filePath[0]['path'] + "'></div>" ).trigger('change');
+	}else{
+		filePaths.forEach(element => {
+			$('.' + requestingField).append( "<div class='uploadImgClose w-25 mr-2'><img src='/" + element + "' alt='' class='uploadImg d-block mb-4 mr-2 w-100'><input type='text' class='d-none' name='img[]' value=' /" + element + "'></div>" ).trigger('change');
+		});
+	}
 }
