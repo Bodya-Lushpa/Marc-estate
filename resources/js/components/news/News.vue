3db3181f071d6a49d316f572dae7bd1b3f7d7a99
Bohdan L <bodya.lushpa@gmail.com>
2022-12-22 00:05:22 +0200

    Add pagennation news

--- resources/js/components/news/News.vue
+++ resources/js/components/news/News.vue
@@ -7,7 +7,7 @@
 
 			<!-- News Block -->
 			<div
-				v-for="post in news"
+				v-for="post in filteredProjects()"
 			 class="news-block col-lg-4 col-md-6 col-sm-12">
 				<div class="inner-box">
 					<div class="image-box">
@@ -31,24 +31,15 @@
 		<!-- Pagination -->
 		<div class="styled-pagination">
 			<ul class="clearfix">
-				<li class="prev">
-					<a href="#"><span>Пред</span></a>
-				</li>
-				<li>
-					<a href="#"><span>1</span></a>
-				</li>
-				<li class="active">
-					<a href="#"><span>2</span></a>
-				</li>
-				<li>
-					<a href="#"><span>3</span></a>
-				</li>
-				<li>
-					<a href="#"><span>4</span></a>
-				</li>
-				<li class="next">
-					<a href="#"><span>След</span></a>
-				</li>
+						<li class="prev" v-if="page > 1" @click="page = page - 1">
+							<a type="click"><span>Пред</span></a>
+						</li>
+						<li v-for="(pageNumber, key) in pageCount" :key="key" :class="[ pageNumber == page ? 'active' : '' ]" @click="page = pageNumber">
+							<a type="click"><span>{{ pageNumber }}</span></a>
+						</li>
+						<li class="next" v-if="hasNextPage" @click="page = page + 1">
+							<a type="submit"><span>След</span></a>
+						</li>
 			</ul>
 		</div>
 	</div>
@@ -64,7 +55,10 @@ import moment from 'moment';
 export default {
 	data(){
 		return {
-			news: []
+			news: [],
+			page: 1,
+			pageCount: 1,
+			hasNextPage: true,
 		}
 	},
 	mounted (){
@@ -72,6 +66,16 @@ export default {
 		.then(response => {
 			this.news = response.data;
 		})
+	},
+	methods: {
+		filteredProjects() {
+			const start = (this.page - 1) * 6;
+			const end = this.page * 6;
+			this.pageCount = Math.ceil(this.news.length/6);
+			this.hasNextPage = this.news.length > end;
+
+			return this.news.slice(start, end);
+		}
 	},
 	filters: {
 		moment: function (date) {
